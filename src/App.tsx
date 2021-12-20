24bbd0d5bcf579682b63c84847cf6cb013147c89
dmcclung <35938+dmcclung@users.noreply.github.com>
2021-12-19 20:26:37 -0600

    Project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import { useWeb3 } from '@3rdweb/hooks'
+import './App.css'
 
 function App() {
+  const { connectWallet, address, error, provider } = useWeb3()
+
+  if (address) {
+    console.log('Connected address', address)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="logo-pixel-cherry"></div>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to CandyDAO
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!address && (
+          <button onClick={() => connectWallet('injected')}>Connect</button>
+        )}
+        {address && (
+          <h1>connected</h1>
+        )}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
